cabb6707f67bcb6bda821126d90e392098b9a650
Arelias <robertmusialik95@gmail.com>
2019-08-07 21:14:35 +0200

    Basic foundations

---
+++ src/main/java/Board.java
@@ -0,0 +1,27 @@
+import javafx.scene.layout.GridPane;
+
+public class Board {
+
+    GridPane board = new GridPane();
+
+
+
+
+    private Board(){}
+
+    public GridPane getBoard() {
+        return board;
+    }
+
+    public void setBoard(GridPane board) {
+        this.board = board;
+    }
+
+    //Use prototype
+
+
+
+
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,50 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    //Constants
+    Integer WINDOW_WIDTH = 1000;
+    Integer WINDOW_HEIGHT = 500;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello World");
+        GridPane test = new GridPane();
+        Scene scene = new Scene(test, WINDOW_WIDTH, WINDOW_HEIGHT, Color.BLACK);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+//public class BoardGameMain extends Application {
+//
+//    private Image imageback = new Image("background.jpg");
+//    //private Optional<Pawn> selectedPawn = Optional.empty();
+//    private Pawn selectedPawn = null;
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//
+//
+//    //Create splitpane
+//    //top part for data shit
+//    //bottom part for game shit
+//    @Override
+//    public void start(Stage primaryStage) throws Exception {
+//        BackgroundSize backgroundSize = new BackgroundSize(imageback.getWidth(), imageback.getHeight(), true, true, true, false);
+//        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+//        Background background = new Background(backgroundImage);
